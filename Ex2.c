233ef69e5612175ed2ce816794cd2e7c84822a58
student <student@g01-13.std.sliita.metro>
2020-01-18 21:39:26 +0530

    C program that takes integers as inputs for an array of size 10 , and another value for the variable V. The program search if V is in the array of 10 integers. The program writes: V is in the array or V is not in the array

---
+++ Ex2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int array[10];
+	int v;
+	for(int i=0;i<10;i++){
+		printf("Enter the array[%d] value: ",i);
+		scanf("%d",&array[i]);
+	}
+	 printf("Enter the v value:");
+         scanf("%d",&v);
+
+	for(int i=0;i<10;i++){
+		if(array[i]==v){
+			printf("v is in the array");
+			return 0;	//return zero to exit the program
+		}
+	}
+	printf("v is not in the array\n");
+	
+}
